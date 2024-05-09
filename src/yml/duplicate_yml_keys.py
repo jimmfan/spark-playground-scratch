e6910b7218ba7652febb5860ffbe3e13ae232326
jimmfan <jimmfan@users.noreply.github.com>
2024-05-09 14:37:28 -0500

    updated to importlib resources

--- src/yml/duplicate_yml_keys.py
+++ src/yml/duplicate_yml_keys.py
@@ -1,5 +1,6 @@
 import yaml
 from yaml.loader import SafeLoader
+import importlib.resources as pkg_resources  # Use importlib_resources for Python<3.7
 
 class UniqueKeyLoader(SafeLoader):
     def construct_mapping(self, node, deep=False):
@@ -12,13 +13,15 @@ class UniqueKeyLoader(SafeLoader):
                 mapping[key] = self.construct_object(value_node, deep=deep)
         return mapping
 
-def check_yaml(filename):
+def check_yaml_from_package(package, resource):
     try:
-        with open(filename, 'r') as file:
+        with pkg_resources.open_text(package, resource) as file:
             yaml.load(file, Loader=UniqueKeyLoader)
         print("No duplicate keys found.")
     except ValueError as e:
         print(e)
+    except Exception as e:
+        print("Failed to read YAML:", e)
 
 # Example usage
-check_yaml('yourfile.yaml')
+check_yaml_from_package('your_package_name', 'yourfile.yaml')
