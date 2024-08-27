28eee7d278d16815a34af60a2f36abc62fcb5ea9
jimmfan <jimmfan@users.noreply.github.com>
2024-08-27 11:33:14 -0500

    Added basic unit test template example

---
+++ src/unit_test/unit_test_example.py
@@ -0,0 +1,27 @@
+import unittest
+
+def reverse_string(s: str) -> str:
+    # This function will be implemented by the developers
+    pass
+
+class TestReverseString(unittest.TestCase):
+    def test_basic(self):
+        self.assertEqual(reverse_string("hello"), "olleh")
+
+    def test_empty(self):
+        self.assertEqual(reverse_string(""), "")
+
+    def test_spaces(self):
+        self.assertEqual(reverse_string("open ai"), "ia nepo")
+
+    def test_punctuation(self):
+        self.assertEqual(reverse_string("a.b,c!"), "!,c.b.a")
+
+    def test_numbers(self):
+        self.assertEqual(reverse_string("12345"), "54321")
+
+    def test_mixed(self):
+        self.assertEqual(reverse_string("Python 3.9"), "9.3 nohtyP")
+
+if __name__ == "__main__":
+    unittest.main()
