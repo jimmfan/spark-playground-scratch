1b583a60cfb76fa262e06bb023f67d0c6885171d
jimmfan <jimmfan@users.noreply.github.com>
2024-10-24 11:07:36 -0500

    added missing function

--- src/pdf/preprocess_img.py
+++ src/pdf/preprocess_img.py
@@ -1,5 +1,25 @@
 import cv2
+import numpy as np
 
+# Define a sharpening kernel
+sharpening_kernel = np.array([[-1, -1, -1], 
+                              [-1, 9, -1],
+                              [-1, -1, -1]])
+
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h),
+                             flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
 
 img = cv2.imread('path/to/image.jpg', cv2.IMREAD_GRAYSCALE)
 denoised_img = cv2.medianBlur(img, 5)
