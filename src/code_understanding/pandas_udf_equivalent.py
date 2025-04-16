a4beea71e799b9f4051a6c469eed242e6316fdb9
jimmfan <jimmfan@users.noreply.github.com>
2025-04-15 21:13:34 -0500

    Updated to spark 2.4

--- src/code_understanding/pandas_udf_equivalent.py
+++ src/code_understanding/pandas_udf_equivalent.py
@@ -4,29 +4,35 @@ from datetime import datetime
 DEFAULT_TIMESTAMP = datetime.max
 DEFAULT_INT = -1
 
-df = df.withColumns({
-    "min_date": F.when(
-        F.col("col1").isNull() & F.col("col2").isNull() & F.col("col3").isNull(),
-        None
-    ).otherwise(
-        F.least(
-            F.coalesce(F.col("col1"), F.lit(DEFAULT_TIMESTAMP)),
-            F.coalesce(F.col("col2"), F.lit(DEFAULT_TIMESTAMP)),
-            F.coalesce(F.col("col3"), F.lit(DEFAULT_TIMESTAMP))
+df = (
+    df.withColumn(
+        "min_date",
+        F.when(
+            F.col("col1").isNull() & F.col("col2").isNull() & F.col("col3").isNull(),
+            None
+        ).otherwise(
+            F.least(
+                F.coalesce(F.col("col1"), F.lit(DEFAULT_TIMESTAMP)),
+                F.coalesce(F.col("col2"), F.lit(DEFAULT_TIMESTAMP)),
+                F.coalesce(F.col("col3"), F.lit(DEFAULT_TIMESTAMP))
+            )
         )
-    ),
-    
-    "months_between1": F.floor(F.months_between(F.col("min_date"), F.col("application_date"))),
-    
-    "months_between2": F.floor(F.months_between(F.col("min_date"), F.col("some_other_date"))),
-    
-    "max_months_between": F.when(
-        F.col("months_between1").isNull() & F.col("months_between2").isNull(),
-        None
-    ).otherwise(
-        F.greatest(
-            F.coalesce(F.col("months_between1"), F.lit(DEFAULT_INT)),
-            F.coalesce(F.col("months_between2"), F.lit(DEFAULT_INT))
+    ).withColumn(
+        "months_between1",
+        F.floor(F.months_between(F.col("min_date"), F.col("application_date")))
+    ).withColumn(
+        "months_between2",
+        F.floor(F.months_between(F.col("min_date"), F.col("some_other_date")))
+    ).withColumn(
+        "max_months_between",
+        F.when(
+            F.col("months_between1").isNull() & F.col("months_between2").isNull(),
+            None
+        ).otherwise(
+            F.greatest(
+                F.coalesce(F.col("months_between1"), F.lit(DEFAULT_INT)),
+                F.coalesce(F.col("months_between2"), F.lit(DEFAULT_INT))
+            )
         )
     )
-})
+)
